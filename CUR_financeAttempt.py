5cb48ede23137215121c43d66057c8d15581510c
zackattacknyu <zackattacknyu@gmail.com>
2017-02-16 02:37:51 -0800

    DF

--- CUR_financeAttempt.py
+++ CUR_financeAttempt.py
@@ -65,39 +65,37 @@ import csv
 origNet = ResNet50(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
 net = Model(input=origNet.input,output=origNet.get_layer('flatten_1').output)
 """
-patFile = "/home/zdestefa/financeProj/TrainTestData1.mat"
+patFile = "/home/zdestefa/financeProj/TrainTestData2.mat"
 curMATcontent = sio.loadmat(patFile)
-x = curMATcontent["Xdata"]
+x = curMATcontent["XtrainOther"]
+y2Given = curMATcontent["yDataCategory"]
 yGiven = curMATcontent["Ydata"]
-y = np.reshape(yGiven,(499))
+y = np.reshape(yGiven,(498))
+y2 = np.reshape(y2Given,(498))+1
 
-trn_x, val_x, trn_y, val_y = cross_validation.train_test_split(x, y, random_state=42, stratify=y,
-                                                                   test_size=0.20)
+#trn_x, val_x, trn_y, val_y = cross_validation.train_test_split(x, y, random_state=42, stratify=y,test_size=0.20)
+trn_x, val_x, trn_y, val_y = cross_validation.train_test_split(x, y, random_state=42, test_size=0.20)
+trn_x2, val_x2, trn_y2, val_y2 = cross_validation.train_test_split(x, y2, random_state=42, test_size=0.20)
+clf = xgb.XGBRegressor(max_depth=10, n_estimators=300, min_child_weight=9,learning_rate=0.05,nthread=8,subsample=0.80,colsample_bytree=0.80,seed=4242)
+#clf = xgb.XGBClassifier(max_depth=10, n_estimators=300, min_child_weight=9,learning_rate=0.05,nthread=8,subsample=0.80,colsample_bytree=0.80,seed=4242)
 
-clf = xgb.XGBRegressor(max_depth=10,
-                       n_estimators=1500,
-                       min_child_weight=9,
-                       learning_rate=0.05,
-                       nthread=8,
-                       subsample=0.80,
-                       colsample_bytree=0.80,
-                       seed=4242)
-
-clf.fit(trn_x, trn_y, eval_set=[(val_x, val_y)], verbose=True, eval_metric='mae', early_stopping_rounds=100)
-
+#TODO: INVESTIGATE FURTHUR THE RESULTS FROM HERE
+clf.fit(trn_x, trn_y, eval_set=[(val_x, val_y)], verbose=True, eval_metric='rmse', early_stopping_rounds=100)
+"""
 y_pred = clf.predict(val_x)
 predictions = [round(value) for value in y_pred]
 # evaluate predictions
 accuracy = accuracy_score(val_y, predictions)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
-yTrain = np_utils.to_categorical(trn_y, 2)
-yVal = np_utils.to_categorical(val_y, 2)
-input_img = Input(shape=(70,))
-layer1 = Dense(32, activation='relu')(input_img)
-layer2 = Dense(32, activation='sigmoid')(input_img)
-outputLayer = Dense(2, init='normal',activation='softmax')(layer2)
+yTrain2 = np_utils.to_categorical(trn_y2, 3)
+yVal2 = np_utils.to_categorical(val_y2, 3)
+input_img = Input(shape=(11,))
+layer1 = Dense(6, activation='relu')(input_img)
+layer2 = Dense(6, activation='sigmoid')(input_img)
+outputLayer = Dense(3, init='normal',activation='softmax')(layer2)
 nnModel = Model(input = input_img,output=outputLayer)
 nnModel.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-nnModel.fit(trn_x, yTrain, batch_size=50, nb_epoch=100,
-                  verbose=1, validation_data=(val_x, yVal))
+nnModel.fit(trn_x2, yTrain2, batch_size=50, nb_epoch=100,
+                  verbose=1, validation_data=(val_x2, yVal2))
+"""
--- setupDataForNN.m
+++ setupDataForNN.m
@@ -2,15 +2,17 @@ load('SAP500DataForOneYear.mat');
 
 %training set will be Jan 10 - Jan 29 data
 
-%target will be predicting whether closing price on February 5 2010
+%target will be predicting whether closing price on February 10 2010
 %   was higher or lower than closing price on January 29 2010
 
 trainingInds = find(Date>20100110 & Date<20100200);
-targetInds = find(Date==20100129 | Date==20100205);
+targetInds = find(Date==20100129 | Date==20100210);
 
 symbols = unique(Symbol);
 trainData = cell(1,length(symbols));
 trainDataTarget = zeros(1,length(symbols));
+yDataCategory = zeros(1,length(symbols));
+XtrainOther = zeros(length(symbols),11);
 goodInds = [];
 for ii = 1:length(symbols)
     curSymbol = symbols(ii);
@@ -22,7 +24,13 @@ for ii = 1:length(symbols)
     end
     goodInds = [goodInds ii];
     targetData = Close(curTargetInds);
-    trainDataTarget(ii) = (targetData(2)>targetData(1));
+    currentY = (targetData(2)-targetData(1))/targetData(1);
+    trainDataTarget(ii) = currentY;
+    if(currentY > 0.05)
+        yDataCategory(ii)=1;
+    elseif(currentY < -0.05)
+        yDataCategory(ii)=-1;
+    end
     curTrain = zeros(length(curTrainInds),5);
     openPrice = Open(curTrainInds(1));
     curTrain(:,1)=Open(curTrainInds);
@@ -34,8 +42,24 @@ for ii = 1:length(symbols)
     
     curTrain(:,5)=Volume(curTrainInds);
     trainData{ii} = curTrain;
+    
+    XtrainOther(ii,1)=min(curTrain(:,1));
+    XtrainOther(ii,2)=max(curTrain(:,1));
+    XtrainOther(ii,3)=mean(curTrain(:,1));
+    
+    XtrainOther(ii,4)=min(curTrain(:,2));
+    XtrainOther(ii,5)=max(curTrain(:,2));
+    XtrainOther(ii,6)=mean(curTrain(:,2));
+    
+    XtrainOther(ii,7)=min(curTrain(:,5));
+    XtrainOther(ii,8)=max(curTrain(:,5));
+    XtrainOther(ii,9)=mean(curTrain(:,5));
+    
+    XtrainOther(ii,10)=min(curTrain(:,3));
+    XtrainOther(ii,11)=max(curTrain(:,4));
 end
-%%
+
+XtrainOther = XtrainOther(goodInds,:);
 trainDataAdj = trainData(goodInds);
 Xdata = zeros(length(trainDataAdj),numel(trainDataAdj{1}));
 for i = 1:length(trainDataAdj)
@@ -43,5 +67,15 @@ for i = 1:length(trainDataAdj)
     Xdata(i,:) = curFeat(:)';
 end
 Ydata = trainDataTarget(goodInds);
+yDataCategory = yDataCategory(goodInds);
 
-save('TrainTestData1.mat','Xdata','Ydata');
+save('TrainTestData2.mat','XtrainOther','Xdata','Ydata','yDataCategory');
+%%
+
+%rmse if random guess
+YdataRand = randn(size(Ydata)).*0.025;
+sqrt(sum((Ydata-YdataRand).^2)/length(Ydata))
+%rmse of random guess: 0.0524
+%rmse with xgboost: 0.03
+
+%continue with this trend 
