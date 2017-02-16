21a05b3510addbd76e203c855d94aae4e6c64f7f
zackattacknyu <zackattacknyu@gmail.com>
2017-02-15 22:49:23 -0800

    df

--- dividendScript.m
+++ dividendScript.m
@@ -21,6 +21,42 @@ pricePerShare=[
 17.11;
 18.32];
 
-totalInvested=300;
+earningsPerDollar = earningsPerShare./pricePerShare;
+[~,stockInd] = max(earningsPerDollar);
+totalInvested=500;
+moneySpent = 0;
+moneyLeft = totalInvested-moneySpent;
 
-[X,FVAL,EXITFLAG,OUTPUT] = linprog(earningsPerShare,pricePerShare',totalInvested)
+[vals,indsInOrder] = sort(earningsPerDollar,'descend');
+numStocks = zeros(size(earningsPerDollar));
+for i = 1:length(numStocksPossible)
+   curNumStocks = floor(moneyLeft/pricePerShare(indsInOrder(i)));
+   moneySpent = moneySpent + curNumStocks*pricePerShare(indsInOrder(i));
+   numStocks(indsInOrder(i))=numStocks(indsInOrder(i))+curNumStocks;
+   moneyLeft = totalInvested-moneySpent;
+end
+
+totalEarnings = sum(numStocks.*earningsPerShare)
+totalCost = sum(numStocks.*pricePerShare)
+%{
+numStocksPossible = floor(300./pricePerShare);
+
+earnings = earningsPerShare(stockInd)*numStocksPossible(stockInd)
+moneySpent = pricePerShare(stockInd)*numStocksPossible(stockInd)
+
+
+
+moneySpent = 0;
+curValInd = 1;
+
+while(moneySpent < totalInvested)
+    curStockInd = indsInOrder(curValInd);
+    moneyLeft = totalInvested-moneySpent;
+    while(moneyLeft > pricePerShare(curStockInd))
+        moneySpent = moneySpent + pricePerShare(curStockInd);
+        moneyLeft = totalInvested-moneySpent;
+        numStocks(curStockInd)=numStocks(curStockInd)+1;
+    end
+    curStockInd = curStockInd+1;
+end
+%}
